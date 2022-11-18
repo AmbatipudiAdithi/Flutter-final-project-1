24d9aed4d830a4f2a4272c15518a63bb6fa62380
Harsha-254 <harsha.vishwanath2004@gmail.com>
2022-11-18 22:05:10 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: myHomePage());
+  }
+}
+
+class myHomePage extends StatefulWidget {
+  const myHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<myHomePage> createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("",style: TextStyle(
+          
+        ),),
+        centerTitle: true,
+      ),
+    );
+  }
+}

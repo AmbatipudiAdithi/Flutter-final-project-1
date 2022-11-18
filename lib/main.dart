c9c40dfd6c0eebcbb93d5de7d298f447372a7fb0
Harsha-254 <harsha.vishwanath2004@gmail.com>
2022-11-18 22:32:19 +0530

    Additions, issue detected with page 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:final_project/secondpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,16 +22,48 @@ class myHomePage extends StatefulWidget {
 }
 
 class _myHomePageState extends State<myHomePage> {
+  var items=[
+    'Triangle','Square','Rectangle','Trapezium','Circle'
+  ];
+  var choice,x;
+  nextpage(){
+    Navigator.push(context,MaterialPageRoute(builder: (context)=>secondpage(choice: choice)));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("",style: TextStyle(
-          
+        title: Text("Area Calculator",style: TextStyle(
+          color: Colors.white,fontSize: 28,fontStyle: FontStyle.italic
         ),),
         centerTitle: true,
       ),
+      body:Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text("Please select your choice of figure",style: TextStyle(
+                color: Colors.white,fontStyle: FontStyle.italic,fontSize: 23
+              ),),
+              DropdownButton(items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+                onChanged: choice=x,
+                value: x,
+              ),
+              ElevatedButton(onPressed: nextpage(), child: Text("Click to input values",style: TextStyle(
+                color: Colors.white,fontSize: 23,fontStyle: FontStyle.italic
+              ),))
+            ],
+          )
+        ],
+      )
     );
   }
 }
---
+++ lib/secondpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:final_project/main.dart';
+
+class secondpage extends StatefulWidget {
+  secondpage({required this.choice});
+  final choice;
+
+  @override
+  State<secondpage> createState() => _secondpageState();
+}
+
+class _secondpageState extends State<secondpage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}

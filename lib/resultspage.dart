74af25bb0b96370258de1b2b805bdb21d745f875
Harsha-254 <harsha.vishwanath2004@gmail.com>
2022-11-19 06:26:04 +0530

    Errors in tranferring data between pages

--- lib/resultspage.dart
+++ lib/resultspage.dart
@@ -17,22 +17,46 @@ class _resultspageState extends State<resultspage> {
   Widget build(BuildContext context) {
     var choice=widget.d;
     var a,p;
-    results(){
-      if (choice=='Square'){
-        a=widget.a*widget.a;
-        p=4*widget.a;
-      }else if (choice=='Rectangle'){
-        a=widget.a*widget.b;
-        p=2*(widget.a+widget.b);
-      }else if (choice=='Triangle'){
-        p=widget.a+widget.b+widget.c;
-        var s=(p/2)*100.roundToDouble()/100;
-        var x=s*(s-widget.a)*(widget.b)*(widget.c);
-        a=pow(x, 0.5);
-      }
+    if (choice=='Square'){
+    a=widget.a*widget.a;
+    p=4*widget.a;
+    }else if (choice=='Rectangle'){
+    a=widget.a*widget.b;
+    p=2*(widget.a+widget.b);
+    }else if (choice=='Triangle'){
+    p=widget.a+widget.b+widget.c;
+    var s=(p/2)*100.roundToDouble()/100;
+    var x=s*(s-widget.a)*(widget.b)*(widget.c);
+    a=pow(x, 0.5);
+    }else if (choice=='Circle'){
+    var pi=3.14;
+    a=pi*widget.a*widget.a;
+    p=pi*2*widget.a;
     }
     return Scaffold(
-      
+        appBar: AppBar(
+          title: Text("Area Calculator",style: TextStyle(
+              color: Colors.white,fontSize: 28,fontStyle: FontStyle.italic
+          ),),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.black,
+        body:Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text("Area: "+a.toString(),style: TextStyle(
+                    color: Colors.black,fontStyle: FontStyle.italic,fontSize: 24
+                )),
+                Text("Perimeter: "+p.toString(),style: TextStyle(
+                    color: Colors.black,fontStyle: FontStyle.italic,fontSize: 24
+                ))
+              ],
+            )
+          ],
+        )
     );
   }
 }
